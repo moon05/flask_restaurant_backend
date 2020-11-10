677b161eb0c2f352e01f0961fac052e5b31c2c52
Abdullah Al Mamun <amamun@u.rochester.edu>
2020-11-10 13:32:15 -0500

    a

--- app.py
+++ app.py
@@ -211,6 +211,6 @@ def get_dist_tts():
 		result[j] = z
 	return result
 
-port = int(os.environ.get('PORT', 3307))
+port = int(os.environ.get('PORT', 5000))
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
